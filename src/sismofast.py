3aa55e1062ef182b19757a716cf7698b50c04976
JoseMariaGarciaMarquez <josemariagarciamarquez2.72@gmail.com>
2024-07-21 06:29:27 -0600

    sismofast mejoras

--- src/sismofast.py
+++ src/sismofast.py
@@ -1,15 +1,27 @@
+""""
+Sismofast: Software para el cálculo de la magnitud de energía de un sismo de manera rápida y eficiente.
+Implementando la función de auto-picking para la detección de la onda P y el cálculo de la magnitud de energía.
+Solo usarse en sismos mayores a 6.0 grados.
+"""
+
 import time
 import numpy as np
 from obspy import read
 
-def auto_picking(z_data, z_time):
+
+def auto_picking(z_data):
+    """
+    Esta función implementa el algoritmo de auto-picking para la detección de la onda P en una señal sísmica.
+    Recibe como entrada los datos de la componente Z de la señal (z_data) y los tiempos correspondientes (z_time).
+    Devuelve el tiempo de llegada de la onda P (p_wave_time) y su amplitud (p_wave_amplitude).
+    """
+
     window_size = 5
     window_step = window_size
 
     num_windows = (len(z_data) - window_size) // window_step + 1
 
     found_wave = False
-    p_wave_time = None
     p_wave_amplitude = None
 
     for i in range(num_windows):
@@ -38,15 +50,12 @@ def auto_picking(z_data, z_time):
             if not found_wave:
                 found_wave = True
                 p_wave_amplitude = np.max(window_next)
-                max_index = np.argmax(window_next)
-                p_wave_time = z_time[start_next + max_index]
 
-    return p_wave_time, p_wave_amplitude
+    return p_wave_amplitude
 
 def momentum0(A, A0):
-    M0_dyne_cm = (A / A0) * 10**23  # dyne-cm
-    M0_Nm = M0_dyne_cm * 1e-7  # Convertir a N m
-    return M0_Nm
+    M0 = (A / A0) * 1e16 #N-m
+    return M0
 
 def magnitude_w(st, A0):
     z_data, n_data, e_data = st[0].data, st[1].data, st[2].data
@@ -58,9 +67,9 @@ def magnitude_w(st, A0):
 
 def magnitude_energy(st):
     tr = st[0]
-    z_data, z_time = tr.data, tr.times()
+    z_data = tr.data
 
-    _, p_wave_amplitude = auto_picking(z_data, z_time)
+    p_wave_amplitude = auto_picking(z_data)
     if p_wave_amplitude is not None:
         A0 = p_wave_amplitude
 
@@ -71,11 +80,22 @@ def magnitude_energy(st):
     else:
         raise ValueError("No se pudo encontrar la amplitud de la onda P")
 
+def magnitude_amplitude(st):
+    z_data, n_data, e_data = st[0].data, st[1].data, st[2].data
+    amp_z, amp_n, amp_e = np.max(z_data), np.max(n_data), np.max(e_data)
+    A = np.sqrt(amp_z**2 + amp_n**2 + amp_e**2)
+    p_wave_amplitude = auto_picking(z_data)
+    if p_wave_amplitude is not None:
+        A0 = p_wave_amplitude
+        M0 = momentum0(A, A0)  # Momento sísmico en N m
+        MA = (np.log10(M0*1e7) - 16.1) / 1.5
+        return MA
+
 def main():
     start_time = time.time() 
     st = read('data/ee140418.o27')
-    magnitude = magnitude_energy(st)
-    print(f"Magnitud de Energía: {round(magnitude,1)}")
+    magnitude = magnitude_amplitude(st)
+    print(f"Magnitud de Amplitud: {round(magnitude,1)}")
 
     end_time = time.time()
     elapsed_time = end_time - start_time
